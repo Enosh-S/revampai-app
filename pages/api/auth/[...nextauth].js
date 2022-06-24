c27efe467d3f79a52d37e02394d5d7be94856169
Enosh-S <enosh@outlook.in>
2022-06-24 13:00:20 +0530

    major changes in login and ui

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,42 +1,42 @@
-import NextAuth from "next-auth"
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import GoogleProvider from "next-auth/providers/google"
-import TwitterProvider from "next-auth/providers/twitter"
-import { FirebaseAdapter }  from "@next-auth/firebase-adapter";
+// import NextAuth from "next-auth"
+// import { initializeApp, getApp, getApps } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import GoogleProvider from "next-auth/providers/google"
+// import TwitterProvider from "next-auth/providers/twitter"
+// import { FirebaseAdapter }  from "@next-auth/firebase-adapter";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCdowZjDeSyOHV0ReMXr09FsCvT8AKKLU8",
-    authDomain: "revamp-ai.firebaseapp.com",
-    projectId: "revamp-ai",
-    storageBucket: "revamp-ai.appspot.com",
-    messagingSenderId: "1090983906436",
-    appId: "1:1090983906436:web:e56fe7380ebaedee9c86d1",
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCdowZjDeSyOHV0ReMXr09FsCvT8AKKLU8",
+//     authDomain: "revamp-ai.firebaseapp.com",
+//     projectId: "revamp-ai",
+//     storageBucket: "revamp-ai.appspot.com",
+//     messagingSenderId: "1090983906436",
+//     appId: "1:1090983906436:web:e56fe7380ebaedee9c86d1",
     
-};
+// };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-const db = getFirestore(app);
-
-
+// const db = getFirestore(app);
 
 
 
 
-export default NextAuth({
-  // Configure one or more authentication providers
-  adapter: FirebaseAdapter(db),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    // TwitterProvider({
-    //     clientId: process.env.TWITTER_CLIENT_ID,
-    //     clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    // }),// ...add more providers here
-  ],
+
+
+// export default NextAuth({
+//   // Configure one or more authentication providers
+//   adapter: FirebaseAdapter(db),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     // TwitterProvider({
+//     //     clientId: process.env.TWITTER_CLIENT_ID,
+//     //     clientSecret: process.env.TWITTER_CLIENT_SECRET,
+//     // }),// ...add more providers here
+//   ],
   
-})
+// })
