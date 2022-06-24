5ccac5e3872efac8385fe499f7c98a38c3afefd4
Enosh-S <enosh@outlook.in>
2022-06-24 10:57:54 +0530

    class name update

--- components/Login.js
+++ components/Login.js
@@ -2,8 +2,8 @@ import { signIn } from "next-auth/react";
 
 export default function Login() {
     return (
-        <div class=" flex flex-col mx-auto items-center h-screen justify-center">
-            <button onClick={signIn} class="btn btn-primary">
+        <div className=" flex flex-col mx-auto items-center h-screen justify-center">
+            <button onClick={signIn} className="btn btn-primary">
              SignIn
             </button>   
         </div>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,21 +4,21 @@ import { ImPilcrow } from "react-icons/im";
 export default function Dashboard() {
     return (
         <div>
-            <div class="px-4 sm-px-6 md:px-8 dark:bg-base-200 shadow-inner-xl">
+            <div className="px-4 sm-px-6 md:px-8 dark:bg-base-200 shadow-inner-xl">
                 <div className="relative max-w-5xl mx-auto py-20 ">
-                    <h1 class="text-slate-900 font-extrabold text-4xl sm:text-5xl lg-text-6xl tracking-tight text-center dark:text-white">
+                    <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg-text-6xl tracking-tight text-center dark:text-white">
                         Revamp your
-                        <span class="text-slate-900 ml-2 font-extrabold text-4xl sm:text-5xl lg-text-6xl tracking-tight text-center dark:text-secondary">
+                        <span className="text-slate-900 ml-2 font-extrabold text-4xl sm:text-5xl lg-text-6xl tracking-tight text-center dark:text-secondary">
                             AI content
                         </span>
                     </h1>
 
-                    <p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
+                    <p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
                         Create a new document and select your usecase
                     </p>
-                    <div class="mt-6 sm:mt-10 flex justify-center">
+                    <div className="mt-6 sm:mt-10 flex justify-center">
                         <a href="/writing">
-                            <button class="btn btn-primary gap-2 text-slate-100 text-base font-sans capitalize">
+                            <button className="btn btn-primary gap-2 text-slate-100 text-base font-sans capitalize">
                                 <ImPilcrow size={15} />
                                 New Document
                             </button>
@@ -29,7 +29,7 @@ export default function Dashboard() {
                 </div>
             </div>
             <section>
-                <div class="max-w-5xl mx-auto relative py-10">
+                <div className="max-w-5xl mx-auto relative py-10">
 
                 </div>
             </section>
